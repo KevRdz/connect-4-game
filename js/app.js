14c01e4563782584b3362da49a6edc1bb035982e
Kevin <kevin.rodriguez678@gmail.com>
2022-10-05 16:45:40 -0400

    create tiles and create the initialize function

--- js/app.js
+++ js/app.js
@@ -8,3 +8,24 @@ let board
 let rows = 6
 let columns = 7
 
+window.onload = function(){
+  init()
+}
+
+function init(){
+  board = []
+
+  for (let r = 0; r < rows; r++){
+    let row = []
+    for (let c = 0; c < columns; c++){
+      row.push(' ')
+
+      let tile = document.createElement("div")
+      tile.id = r.toString() + "-" + c.toString()
+      tile.classList.add("tile")
+      tile.addEventListener("click", setPiece)
+      document.getElementById("board").append(tile)
+    }
+    board.push(row)
+  }
+}
