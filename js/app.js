eee36ea8fa830427f76e9808ec834035ee787776
Kevin <kevin.rodriguez678@gmail.com>
2022-10-10 15:07:08 -0400

    add winning function 4 different ways

--- js/app.js
+++ js/app.js
@@ -60,4 +60,66 @@ function setChip(){
 
   r -= 1;
   currentColumns[c] = r;
+
+  checkWinner();
+}
+
+function checkWinner(){
+  // horizontally
+  for (let r = 0; r < rows; r++){
+    for (let c = 0; c < columns - 3; c++){
+      if (board[r][c]!= ' '){
+        if (board[r][c] == board[r][c+1] && board[r][c+1] == board[r][c+2] && board[r][c+2] == board[r][c+3]){
+          setWinner(r, c)
+          return
+        }
+      }
+    }
+  }
+
+  // vertically
+  for (let c = 0; c < columns; c++){
+    for (let r = 0; r < rows-3; r++){
+      if (board[r][c] != " "){
+        if (board[r][c] == board[r+1][c] && board[r+1][c] == board[r+2][c] && board[r+2][c] == board[r+3][c]){
+          setWinner(r, c)
+          return
+        }
+      }
+    }
+  }
+  // anti diagonally
+  for (let r = 0; r < rows - 3; r++){
+    for (let c = 0; c < columns - 3; c++){
+      if (board[r][c] != " "){
+        if (board[r][c] == board[r+1][c+1] && board[r+1][c+1] == board[r+2][c+2] && board[r+2][c+2] == board[r+3][c+3]){
+          setWinner(r, c)
+          return
+        }
+      }
+    }
+  }
+  // diagonally
+  for (let r = 3; r < rows; r++){
+    for (let c = 0; c < columns - 3; c++){
+      if (board[r][c] != " "){
+        if (board[r][c] == board[r-1][c+1] && board[r-1][c+1] == board[r-2][c+2] && board[r-2][c+2] == board[r-3][c+3]){
+          setWinner(r, c)
+          return
+        }
+      }
+    }
+  }
+
+}
+
+function setWinner(r, c){
+  let winner = document.getElementById("winner")
+  if (board[r][c] == playerRed){
+    winner.innerText = "Red Wins!!"
+  } else {
+    winner.innerText = "Yellow Wins!!"
+  }
+
+  gameOver = true
 }
